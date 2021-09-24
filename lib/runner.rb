6c2d9c79b39402e394e4be0fd9f537d762f79036
Gregory Fischer <gwhoisj@gmail.com>
2021-09-24 11:58:09 -0400

    Feat: basic file i/o

---
+++ lib/runner.rb
@@ -0,0 +1,6 @@
+i_file_name = ARGV[0]
+o_file_name = ARGV[1]
+
+i_file = File.open(i_file_name, 'r')
+o_file = File.open(o_file_name, 'w')
+
---
+++ spec/runner_spec.rb
@@ -0,0 +1,2 @@
+require 'rspec'
+requrie './lib/runner.rb'
